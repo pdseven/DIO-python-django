907a6a9424796076042da2d7a8e3b63fece1f2cb
Diego Neves <pdcassiano@gmail.com>
2020-06-28 18:10:30 -0300

    aula de autenticação e insersão de dados em python-django

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render #, redirect
 from core.models import Evento
 
 
 # Create your views here.
-def evento(titulo_evento):
-    return HttpResponse(Evento.objects.get(titulo=titulo_evento))
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    # evento = Evento.objects.filter(usuario=usuario)
+    response = {'eventos':evento}
+    return render(request, 'agenda.html', response)
