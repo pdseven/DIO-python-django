d105ab4ea6261d0543a7ca129494f0c1d07fd8fc
Diego Neves <pdcassiano@gmail.com>
2020-06-27 21:57:51 -0300

    primeira aplicação django - hello world

---
+++ hello_django/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def hello(request, nome, idade):
+    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+
+def soma(request, num1, num2):
+    return HttpResponse('{} + {} = {}'.format(num1, num2, num1 + num2))
+
+def subtracao(request, num1, num2):
+    return HttpResponse('{} - {} = {}'.format(num1, num2, num1 - num2))
+
+def multiplicacao(request, num1, num2):
+    return HttpResponse('{} * {} = {}'.format(num1, num2, num1 * num2))
+
+
+def divisao(request, num1, num2):
+    return HttpResponse('{} / {} = {}'.format(num1, num2, num1 / num2))
